d54274d844d55af3317e5e268f8d9f21517c1c26
Constantin Michael Weisser <i7c@posteo.de>
2025-12-17 20:08:28 -0300

    Function to update the markdown section

--- src/markdown_access.rs
+++ src/markdown_access.rs
@@ -1,5 +1,48 @@
 use pulldown_cmark::{Event, HeadingLevel, Parser, Tag, TagEnd};
 
+pub fn line_is_heading(line: &str, heading: &str) -> bool {
+    if line.trim().starts_with("#") {
+        line.trim_start_matches(['#', ' ']) == heading
+    } else {
+        false
+    }
+}
+
+pub fn line_is_any_heading(line: &str) -> bool {
+    return line.trim().starts_with("#");
+}
+
+pub fn update_block_strings(block_strings: &Vec<String>, markdown_content: &str) -> String {
+    let mut output_lines: Vec<String> = Vec::new();
+
+    let mut under_heading = false;
+    for l in markdown_content.lines() {
+        if line_is_heading(l, "Time Blocks") {
+            under_heading = true;
+            output_lines.push(l.to_string());
+            output_lines.extend(
+                block_strings
+                    .iter()
+                    .map(|s| s.to_owned())
+                    .collect::<Vec<String>>(),
+            );
+            continue;
+        }
+
+        if line_is_any_heading(l) {
+            under_heading = false;
+        }
+
+        if under_heading {
+            continue;
+        }
+        output_lines.push(l.to_string());
+    }
+
+    output_lines.push("".to_string());
+    output_lines.join("\n")
+}
+
 pub fn read_block_strings(markdown_content: &str) -> Vec<String> {
     let parser = Parser::new(markdown_content);
 
@@ -90,5 +133,70 @@ bla foo
                 "So should this".to_string(),
             ],
         );
+    }
+
+    #[test]
+    fn test_update_blockstrings_in_markdown_with_faulty_elements() {
+        let markdown = "
+# Some Title
+## Some other section
+```clojure
+(+ 1 2)
+```
+
+```tasks
+some other suprising content %#$@
+like $(exit 0)
+```
+bla foo
+   ### Some wrong indent
+## Time Blocks
+- 08:00 - 11:00 This
+- 11:00 - 11:30 should
+- 14:00 - 15:00 appear
+- So should this
+# Notes
+- 10:00 - 11:00 What is this?
+";
+        let block_strings = vec![
+            "- 10:00 - 11:00 (Personal) -hidden-".to_string(),
+            "- 11:00 - 12:00 Meeting".to_string(),
+        ];
+
+        let updated_markdown = update_block_strings(&block_strings, markdown);
+
+        assert_eq!(
+            updated_markdown,
+            "
+# Some Title
+## Some other section
+```clojure
+(+ 1 2)
+```
+
+```tasks
+some other suprising content %#$@
+like $(exit 0)
+```
+bla foo
+   ### Some wrong indent
+## Time Blocks
+- 10:00 - 11:00 (Personal) -hidden-
+- 11:00 - 12:00 Meeting
+# Notes
+- 10:00 - 11:00 What is this?
+"
+        );
+    }
+
+    #[test]
+    fn test_check_for_heading() {
+        assert_eq!(true, line_is_heading("### Foo Bar", "Foo Bar"));
+        assert_eq!(true, line_is_heading(" # Foo Bar", "Foo Bar"));
+    }
+
+    #[test]
+    fn test_line_is_not_heading() {
+        assert_eq!(false, line_is_heading("Foo Bar", "Foo Bar"));
     }
 }
