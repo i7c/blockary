85b36c67b24a74213ad212d25dd493f9f363a8bc
Constantin Michael Weisser <i7c@posteo.de>
2026-01-16 12:12:24 +0100

    Start move functionality to DayPlan

--- src/day_plan.rs
+++ src/day_plan.rs
@@ -7,3 +7,83 @@ pub trait DayPlanTrait {
     fn with_updated_blocks(self, blocks: &Vec<Block>) -> Self;
     fn day(&self) -> Option<NaiveDate>;
 }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Source {
+    ObsMarkDown { abs_path: String, base_dir: String },
+    ICalendar,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DayPlan {
+    pub origin: String,
+    pub blocks: Vec<Block>,
+    pub day: Option<NaiveDate>,
+    source: Source,
+}
+
+impl DayPlan {
+    fn only_original_blocks(self) -> Vec<Block> {
+        self.blocks
+            .iter()
+            .cloned()
+            .filter(|b| b.origin == self.origin)
+            .collect()
+    }
+
+    fn with_updated_blocks(self, blocks: &Vec<Block>) -> Self {
+        let mut updated_blocks: Vec<Block> = blocks.iter().cloned().collect();
+        updated_blocks.sort_by(|a, b| a.period_str.cmp(&b.period_str));
+        DayPlan {
+            blocks: updated_blocks,
+            ..self
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_update_blocks() {
+        let dp1 = DayPlan {
+            origin: "Work".to_string(),
+            blocks: vec![
+                Block {
+                    period_str: "08:00 - 10:30".to_string(),
+                    origin: "Work".to_string(),
+                    desc: "Emails".to_string(),
+                },
+                Block {
+                    period_str: "14:00 - 14:30".to_string(),
+                    origin: "Personal".to_string(),
+                    desc: "Walk".to_string(),
+                },
+            ],
+            day: None,
+            source: Source::ObsMarkDown {
+                abs_path: "/work/20.md".to_string(),
+                base_dir: "/work".to_string(),
+            },
+        };
+
+        let updated = dp1.with_updated_blocks(&vec![Block {
+            period_str: "00:00 - 05:30".to_string(),
+            origin: "Personal".to_string(),
+            desc: "Sleep".to_string(),
+        }]);
+
+        assert_eq!(
+            updated.source,
+            Source::ObsMarkDown {
+                abs_path: "/work/20.md".to_string(),
+                base_dir: "/work".to_string()
+            }
+        );
+        assert_eq!(updated.origin, "Work");
+        assert_eq!(updated.blocks.len(), 1);
+        assert_eq!(updated.blocks.get(0).unwrap().origin, "Personal");
+        assert_eq!(updated.blocks.get(0).unwrap().desc, "Sleep");
+    }
+}
