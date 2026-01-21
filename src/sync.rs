7f245718628c6dc7b18b62653d519100fa160936
Constantin Michael Weisser <i7c@posteo.de>
2026-01-21 21:14:06 +0100

    Refactor load all from config function

--- src/sync.rs
+++ src/sync.rs
@@ -4,20 +4,19 @@ use chrono::NaiveDate;
 use std::collections::HashMap;
 
 pub fn all_day_plans_from_config(config: Config) -> Vec<DayPlan> {
-    config
-        .dirs
-        .iter()
-        .map(|(_, origin)| {
-            let repo = day_plan::DayPlanRepo {
-                name: origin.name.clone(),
-                repo_type: day_plan::DayPlanRepoType::MarkdownDirectory {
-                    dir: origin.path.clone(),
-                },
-            };
-            repo.all()
-        })
-        .flatten()
-        .collect()
+    let mut day_plans = Vec::new();
+
+
+    for (_, origin) in &config.dirs {
+        let repo = day_plan::DayPlanRepo {
+            name: origin.name.clone(),
+            repo_type: day_plan::DayPlanRepoType::MarkdownDirectory {
+                dir: origin.path.clone(),
+            },
+        };
+        day_plans.extend(repo.all());
+    }
+    day_plans
 }
 
 pub fn day_plans_by_day(all_day_plans: Vec<DayPlan>) -> HashMap<NaiveDate, Vec<DayPlan>> {
