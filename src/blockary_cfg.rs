b401d8e3f7486033358504ea4c4791cad5703894
Constantin Michael Weisser <i7c@posteo.de>
2026-01-21 21:10:36 +0100

    Support for cal sections

--- src/blockary_cfg.rs
+++ src/blockary_cfg.rs
@@ -8,8 +8,14 @@ pub struct Dir {
 }
 
 #[derive(Debug, Deserialize)]
+pub struct Cal {
+    pub uri: String,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Config {
     pub dirs: HashMap<String, Dir>,
+    pub cals: Option<HashMap<String, Cal>>,
 }
 
 pub fn load(toml_str: &str) -> Config {
@@ -41,6 +47,30 @@ mod tests {
         assert_eq!(cfg.dirs.get("work").unwrap().name, "Work");
         assert_eq!(cfg.dirs.get("work").unwrap().path, "/home/foo/worknotes");
         assert_eq!(cfg.dirs.len(), 2);
+    }
+
+    #[test]
+    fn test_loading_config_example_with_cals() {
+        let toml_str = r#"
+        [dirs]
+        [dirs.work]
+        path = "/home/foo/worknotes"
+        name = "Work"
+
+        [dirs.personal]
+        path = "/home/bar/baz/personal"
+        name = "Personal"
+
+        [cals]
+        [cals.work]
+        uri = "https://ical.com/x"
+    "#;
+
+        let cfg = load(toml_str);
+        assert_eq!(cfg.dirs.get("work").unwrap().name, "Work");
+        assert_eq!(cfg.dirs.get("work").unwrap().path, "/home/foo/worknotes");
+        assert_eq!(cfg.dirs.len(), 2);
+        assert_eq!(cfg.cals.unwrap().get("work").unwrap().uri, "https://ical.com/x");
     }
 
     #[test]
