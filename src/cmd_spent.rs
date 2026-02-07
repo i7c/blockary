143fc8b103dd9bc0bca0b3ba80861fb8627a390c
Constantin Michael Weisser <i7c@posteo.de>
2026-02-07 18:07:50 -0300

    WIP works

--- src/cmd_spent.rs
+++ src/cmd_spent.rs
@@ -1,3 +1,4 @@
+use crate::block::Block;
 use crate::blockary_cfg;
 use crate::day_plan;
 use crate::day_plan::DayPlanRepo;
@@ -10,18 +11,28 @@ pub fn command(config: blockary_cfg::Config, today: chrono::NaiveDate) {
     }
 }
 
-pub fn time_spent_in_origin(today: chrono::NaiveDate, origin: &blockary_cfg::Dir) {
+
+pub fn time_spent_in_origin(from_inclusive: chrono::NaiveDate, origin: &blockary_cfg::Dir) {
     let repo = DayPlanRepo {
         name: origin.name.clone(),
         repo_type: day_plan::DayPlanRepoType::MarkdownDirectory {
             dir: origin.path.clone(),
         },
     };
+    let all_in_range = repo.all_of_day(from_inclusive);
 
-    let all_of_day = repo.all_of_day(today);
+    let mut all_blocks: Vec<&Block> = Vec::new();
+    for dp in &all_in_range {
+        for block in &dp.blocks {
+            all_blocks.push(&block);
+        }
+    }
+    time_summary::time_per_tag(&all_blocks, 0);
 
+    
+    
     let (hours, minutes) =
-        time_summary::minutes_to_hours_minutes(time_summary::total_time_spent(all_of_day));
+        time_summary::minutes_to_hours_minutes(time_summary::total_time_spent(&all_in_range));
     println!("--:--");
     println!("{:02}:{:02} on {} today", hours, minutes, origin.name);
 }
--- src/time_summary.rs
+++ src/time_summary.rs
@@ -1,4 +1,12 @@
-use crate::day_plan;
+use std::collections::HashMap;
+
+use crate::{block::Block, day_plan};
+
+pub struct TagTime {
+    pub tag: String,
+    pub minutes: u16,
+    pub sub_tags: Vec<TagTime>,
+}
 
 pub fn minutes_to_hours_minutes(total_duration_today: u16) -> (u16, u16) {
     let hours = total_duration_today / 60;
@@ -6,7 +14,38 @@ pub fn minutes_to_hours_minutes(total_duration_today: u16) -> (u16, u16) {
     (hours, minutes)
 }
 
-pub fn total_time_spent(all_of_day: Vec<day_plan::DayPlan>) -> u16 {
+pub fn time_per_tag(all_blocks: &Vec<&Block>, level: usize) -> Vec<TagTime> {
+    let mut groups: HashMap<&str, Vec<&Block>> = HashMap::new();
+    for b in all_blocks {
+        for tag in &b.tags {
+            match tag.tagls.get(level) {
+                Some(tagl) => {
+                    groups.entry(&tagl).or_insert(Vec::new()).push(&b);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    let mut timings = Vec::new();
+    for (tagl, blocks) in groups {
+        let total_accumulated = total_minutes(&blocks);
+
+        println!("{:03} - {} {}", total_accumulated, level, tagl);
+        timings.push(TagTime {
+            tag: tagl.to_string(),
+            minutes: total_accumulated,
+            sub_tags: time_per_tag(&blocks.into(), level + 1),
+        });
+    }
+    timings
+}
+
+pub fn total_minutes(blocks: &Vec<&Block>) -> u16 {
+    blocks.iter().fold(0, |total, b| total + b.duration)
+}
+
+pub fn total_time_spent(all_of_day: &Vec<day_plan::DayPlan>) -> u16 {
     let total_duration_today = all_of_day.iter().fold(0, |total_duration, dp| {
         total_duration
             + dp.only_original_blocks().iter().fold(0, |acc, b| {
